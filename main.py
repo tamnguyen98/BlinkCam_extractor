bfa96f52c9ba25c32827775480fa7ce72e36e17c
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2019-12-19 20:09:25 -0800

    bug fix
    sometime it can't download from the server, so added a feature to re-login and try the download again

--- main.py
+++ main.py
@@ -1,48 +1,57 @@
 from blinkpy import blinkpy
 import datetime
 
-USER = '' # User name to your blink account
-PASS = '' # Password to your blink account
-print('Downloader running...')
+def login(USER, PASS):
+    blink = blinkpy.Blink(username=USER, password=PASS)
+    blink.start()
+    blink.refresh()             # Get new information from server
+    return blink
 
-# Location to store the videos
-blink_dir = 'PATH/TO/STORAGE' # Location to store the videos
-date = datetime.datetime
+if __name__== "__main__":
+    USER = 'user' # User name to your blink account
+    PASS = 'pass' # Password to your blink account
+    print('Downloader running...')
 
-try: # check out directory for file
-    dir_status = open(blink_dir+'last_record', 'r+')
-    info = dir_status.readline()
-except IOError:
-    dir_status = open(blink_dir+'last_record', 'w')
-    info = "2019/12/14 00:00"
-try:
-    old_time =  int(info.split(' ')[1].split(':')[0])
-except ValueError:
-    old_time = int(date.now().strftime("%H"))
+    # Location to store the videos
+    blink_dir = 'PATH/TO/STORAGE/' # Location to store the videos
+    date = datetime.datetime
 
-dir_status.close()
-
-while(True):
-    timer = int(date.now().strftime("%H"))
-    if (timer != old_time): # check if an hr has passed
+    try: # check out directory for file
         dir_status = open(blink_dir+'last_record', 'r+')
-        print("Time difference detected. Old time: ", old_time)
-        # log into blink
-        try:
-            blink = blinkpy.Blink(username=USER, password=PASS)
-            blink.start()
-        except AttributeError:
-            print("Error logging in, retrying...")
-            blink = blinkpy.Blink(username=USER, password=PASS)
-            blink.start()
+        info = dir_status.readline()
+    except IOError:
+        dir_status = open(blink_dir+'last_record', 'w')
+        info = "2019/12/14 00:00"
+    try:
+        old_time =  int(info.split(' ')[1].split(':')[0])
+    except ValueError:
+        old_time = int(date.now().strftime("%H"))
+
+    dir_status.close()
+
+    while(True):
+        timer = int(date.now().strftime("%H"))
+        if (timer != old_time): # check if an hr has passed
+            dir_status = open(blink_dir+'last_record', 'r+')
+            print("Time difference detected. Old time: ", old_time)
+            # log into blink
+            try:
+                blink = login(USER,PASS)
+            except AttributeError:
+                print("Error logging in, retrying...")
+                blink = login(USER,PASS)
 
 
-        blink.refresh()             # Get new information from server
+            try:
+                blink.download_videos('F:/Blink_cam/', since=info)
+            except AttributeError: # If it fails to download, try re-login and retry the download.
+                blink = login(USER,PASS)
+                blink.download_videos('F:/Blink_cam/', since=info)
 
-        blink.download_videos('F:/Blink_cam/', since=info)
-        info = date.now().strftime("%Y/%m/%d %H:%M") # Update last save state
-        dir_status.seek(0)
-        dir_status.write(info)
-        dir_status.truncate()
-        dir_status.close()
-        old_time = timer
+
+            info = date.now().strftime("%Y/%m/%d %H:%M") # Update last save state
+            dir_status.seek(0)
+            dir_status.write(info)
+            dir_status.truncate()
+            dir_status.close()
+            old_time = timer
